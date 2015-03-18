77662ecfe4e58f7c2781051237fd770ac7e4d2eb
Matthew Harrigan <harrigan@stanford.edu>
2015-03-18 15:52:23 -0700

    Copy StateTests from OpenMMCore

--- fahbench/StateTests.h
+++ fahbench/StateTests.h
@@ -1,28 +1,36 @@
-#ifndef STATE_TESTS_H_
-#define STATE_TESTS_H_
+/******************************************************************************\
 
-class OpenMM::State;
+                    Copyright 2013-2015. Stanford University.
+                              All Rights Reserved.
+
+                                Original Author
+                         Yutong Zhao <proteneer@gmail.com>
+
+\******************************************************************************/
+
+#ifndef STATE_TESTS_H
+#define STATE_TESTS_H
+
+namespace OpenMM {class State;}
+
 
 // Single Precision Tolerances
 static double const DEFAULT_FORCE_TOL_KJ_PER_MOL_PER_NM = 5;
 static double const DEFAULT_ENERGY_TOL_KJ_PER_MOL = 10.0;
 
-//static double const DEFAULT_FORCE_TOL_KJ_PER_MOL_PER_NM = 0.3;
-//static double const DEFAULT_ENERGY_TOL_KJ_PER_MOL = 3.0;
-
-// Double Precision Tolerances
-//static double const DEFAULT_FORCE_TOL_KJ_PER_MOL_PER_NM = 0.251; // 0.06 kcal/mol/nm
-//static double const DEFAULT_ENERGY_TOL_KJ_PER_MOL = 0.418; // 0.1 kcal/mol/nm
-// Usage lets us compare the state internals of two platforms to test their differences.
+// Usage lets us compare the state internals of two platforms to test their
+// differences.
 namespace StateTests {
-
-void checkForNans(const OpenMM::State& a);
-void checkForDiscrepancies(const OpenMM::State &a);
-void compareEnergies(const OpenMM::State& a, const OpenMM::State& b, double tolerance = DEFAULT_ENERGY_TOL_KJ_PER_MOL);
-void compareForces(const OpenMM::State& a, const OpenMM::State& b, double tolerance = DEFAULT_FORCE_TOL_KJ_PER_MOL_PER_NM);
-void compareForcesAndEnergies(const OpenMM::State& a, const OpenMM::State &b,
-                              double forceTolerance = DEFAULT_FORCE_TOL_KJ_PER_MOL_PER_NM, double energyTolerance = DEFAULT_ENERGY_TOL_KJ_PER_MOL);
-
+  void checkForNans(const OpenMM::State &a);
+  void checkForDiscrepancies(const OpenMM::State &a);
+  void compareEnergies(const OpenMM::State &a, const OpenMM::State &b,
+                       double tolerance = DEFAULT_ENERGY_TOL_KJ_PER_MOL);
+  void compareForces(const OpenMM::State &a, const OpenMM::State &b,
+                     double tolerance = DEFAULT_FORCE_TOL_KJ_PER_MOL_PER_NM);
+  void compareForcesAndEnergies
+  (const OpenMM::State &a, const OpenMM::State &b,
+   double forceTolerance = DEFAULT_FORCE_TOL_KJ_PER_MOL_PER_NM,
+   double energyTolerance = DEFAULT_ENERGY_TOL_KJ_PER_MOL);
 }
 
-#endif
+#endif // STATE_TESTS_H
