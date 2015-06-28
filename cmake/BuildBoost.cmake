7929e473364e7be3184b83315a8b4e4f633b92cf
Matthew Harrigan <harrigan@stanford.edu>
2015-06-28 05:53:47 -0700

    Use external boost libraries

--- cmake/BuildBoost.cmake
+++ cmake/BuildBoost.cmake
@@ -22,5 +22,19 @@ ExternalProject_Add(boost
         --with-libraries=program_options,filesystem,system
         --prefix=<INSTALL_DIR>
     BUILD_IN_SOURCE 1
-    INSTALL_COMMAND ./b2 install
+    INSTALL_COMMAND
+        ./b2 install
+        cxxflags=-fPIC
+        link=static
 )
+
+ExternalProject_Get_Property(boost install_dir)
+set(BOOST_LIBROOT "${install_dir}/lib")
+set(BOOST_INCLUDE_DIR "{install_dir}/include")
+set(BOOST_LIBRARIES
+    "${BOOST_LIBROOT}/${CMAKE_STATIC_LIBRARY_PREFIX}boost_program_options${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    "${BOOST_LIBROOT}/${CMAKE_STATIC_LIBRARY_PREFIX}boost_filesystem${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    "${BOOST_LIBROOT}/${CMAKE_STATIC_LIBRARY_PREFIX}boost_system${CMAKE_STATIC_LIBRARY_SUFFIX}"
+)
+
+    
