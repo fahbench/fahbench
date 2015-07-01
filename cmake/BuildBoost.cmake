815da71a03ce62ff400b279c9e2b6d826c1d598f
Matthew Harrigan <harrigan@stanford.edu>
2015-06-30 20:32:54 -0700

    Install boost from an archive
    Conflicts:
    	cmake/BuildBoost.cmake

--- cmake/BuildBoost.cmake
+++ cmake/BuildBoost.cmake
@@ -1,27 +1,14 @@
 include(ExternalProject)
 
-set(BOOST_TAG boost-1.58.0
-    CACHE STRING "Git tag to use when building boost")
-
-
 ExternalProject_Add(boost
-    GIT_REPOSITORY https://github.com/boostorg/boost.git
-    GIT_TAG ${BOOST_TAG}
-    GIT_SUBMODULES
-        tools/build
-        tools/inspect
-        libs/program_options
-        libs/filesystem
-        libs/system
-        libs/config # ?
-        libs/wave   # ?
-    UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND 
+    URL http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz
+    URL_MD5 5a5d5614d9a07672e1ab2a250b5defc5
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND
         ./bootstrap.sh
         --with-libraries=program_options,filesystem,system
         --prefix=<INSTALL_DIR>
-    BUILD_IN_SOURCE 1
+    BUILD_COMMAND ""
     INSTALL_COMMAND
         ./b2 install
         cxxflags=-fPIC
@@ -30,7 +17,8 @@ ExternalProject_Add(boost
 
 ExternalProject_Get_Property(boost install_dir)
 set(BOOST_LIBROOT "${install_dir}/lib")
-set(BOOST_INCLUDE_DIR "{install_dir}/include")
+set(BOOST_INCLUDE_DIR "${install_dir}/include")
+
 set(BOOST_LIBRARIES
     "${BOOST_LIBROOT}/${CMAKE_STATIC_LIBRARY_PREFIX}boost_program_options${CMAKE_STATIC_LIBRARY_SUFFIX}"
     "${BOOST_LIBROOT}/${CMAKE_STATIC_LIBRARY_PREFIX}boost_filesystem${CMAKE_STATIC_LIBRARY_SUFFIX}"
