ae161f0889c2a8dd1f16e7d52a6ed55728ca3351
Matthew Harrigan <harrigan@stanford.edu>
2015-07-14 20:35:22 -0700

    Flush

--- fahbench/cmd/CommandLineUpdater.cpp
+++ fahbench/cmd/CommandLineUpdater.cpp
@@ -7,15 +7,15 @@ void CommandLineUpdater::progress(int i, int numSteps, double ns_per_day) {
         return;
     }
     double percent = (100.0 * i) / numSteps;
-    std::cout << boost::format("Progress: %1$5.1f%%  estimate: %2$6.2f ns/day") % percent % ns_per_day << "\r";
+    std::cout << boost::format("Progress: %1$5.1f%%  estimate: %2$6.2f ns/day") % percent % ns_per_day << "\r" << std::flush;
 }
 
 
 void CommandLineUpdater::message(std::string s) {
-    std::cout << s << std::endl;
+    std::cout << s << std::endl << std::flush;
 }
 
 void CommandLineUpdater::message(boost::format f) {
-    std::cout << f << std::endl;
+    std::cout << f << std::endl << std::flush;
 
 }
