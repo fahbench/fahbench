ff63cae9d0a926f30fe82a297a817fbb56796240
Matthew Harrigan <harrigan@stanford.edu>
2015-12-18 14:12:29 -0800

    Add Application Icon

--- fahbench/gui/gui-main.cpp
+++ fahbench/gui/gui-main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QString>
+#include <QIcon>
 
 #include "../FAHBenchVersion.h"
 #include "MainWindow.h"
@@ -8,6 +9,7 @@ int main(int argc, char * argv[]) {
     QApplication app(argc, argv);
     app.setOrganizationName("Folding@Home");
     app.setApplicationName("FAHBench");
+    app.setWindowIcon(QIcon(":FahIcon.ico"));
 
     app.setApplicationVersion(QString::fromStdString(getVersion()));
     MainWindow main_win;
