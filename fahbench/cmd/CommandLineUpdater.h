7461a77bca769a8b27bbc36c2fdfe5bf3c99a1a2
Matthew Harrigan <harrigan@stanford.edu>
2016-10-08 13:56:55 -0700

    Format

--- fahbench/cmd/CommandLineUpdater.h
+++ fahbench/cmd/CommandLineUpdater.h
@@ -1,13 +1,16 @@
 #ifndef COMMANDLINEUPDATER_H
 #define COMMANDLINEUPDATER_H
 
-#include "../Updater.h"
+#include "Updater.h"
 
 class CommandLineUpdater : public Updater {
 public:
     void progress(int i, int numSteps, float ns_per_day) const;
+
     void message(std::string) const;
+
     void message(boost::format) const;
+
     bool cancelled() const;
 };
 
