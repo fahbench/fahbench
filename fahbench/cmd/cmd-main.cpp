6813056eb72308011d6b130f9afe00050a2b93ff
Matthew Harrigan <harrigan@stanford.edu>
2016-10-08 13:54:52 -0700

    Formatting

--- fahbench/cmd/cmd-main.cpp
+++ fahbench/cmd/cmd-main.cpp
@@ -34,7 +34,8 @@ string getGpuDesc() {
         // OpenCL
         auto opencl_devices = GPUInfo::getOpenCLDevices();
         for (auto const & kv : opencl_devices) {
-            ss << fmt % kv.device() % "OpenCL" % kv.device_id() % kv.platform_id() % kv.platform_version % kv.device_version;
+            ss << fmt % kv.device() % "OpenCL" % kv.device_id() % kv.platform_id()
+                  % kv.platform_version % kv.device_version;
         }
     } catch (const std::exception & err) {
         ss << err.what() << std::endl;
@@ -59,7 +60,7 @@ string listWus() {
     ss << fmt % "WU" % "Full name" % "Chunk" % "Description" << std::endl;
     ss << string(80, '-') << std::endl;
     for (auto & wu : WorkUnit::available_wus()) {
-        ss << fmt % wu.codename() %  wu.fullname() % wu.step_chunk() % wu.description() << std::endl;
+        ss << fmt % wu.codename() % wu.fullname() % wu.step_chunk() % wu.description() << std::endl;
     }
     return ss.str();
 }
@@ -70,52 +71,26 @@ int main(int argc, char ** argv) {
 
     po::options_description desc("FAHBench options");
     desc.add_options()
-    ("help,h",
-     "produce help message")
-    ("version,v",
-     "version information")
-    ("device-info,d",
-     "List GPU device information")
-    ("list-wus,l",
-     "List available work units (WUs)")
-    ("device-id",
-     po::value<int>(&simulation.deviceId)->default_value(0),
-     "GPU Device index")
-    ("platform-id",
-     po::value<int>(&simulation.platformId)->default_value(0),
-     "Platform index (OpenCL only)")
-    ("platform",
-     po::value<string>(&simulation.platform)->default_value("OpenCL"),
-     "Platform name (CPU, OpenCL, or CUDA)")
-    ("precision",
-     po::value<string>(&simulation.precision)->default_value("single"),
-     "Precision (single or double)")
-    ("workunit,w",
-     po::value<string>()->default_value("dhfr"),
-     "Work unit (WU) name.")
-    ("system",
-     po::value<string>()->default_value(""),
-     "Path to system xml file")
-    ("state",
-     po::value<string>()->default_value(""),
-     "Path to state xml file")
-    ("integrator",
-     po::value<string>()->default_value(""),
-     "Path to integrator xml file")
-    ("disable-accuracy-check",
-     "Don't check against the reference platform")
-    ("enable-accuracy-check",
-     "Check against the reference platform (default)")
-    ("nan-check",
-     po::value<int>(&simulation.nan_check_freq)->default_value(0),
-     "Frequency to perform NaN checks during benchmark. 0 - disable.")
-    ("run-length",
-     po::value<int>(&run_length_sec)->default_value(60),
-     "Benchmark for this many seconds")
-    ("step-chunk",
-     po::value<int>(),
-     "Run this many steps at a time before updating progress.")
-    ;
+            ("help,h", "produce help message")
+            ("version,v", "version information")
+            ("device-info,d", "List GPU device information")
+            ("list-wus,l", "List available work units (WUs)")
+            ("device-id", po::value<int>(&simulation.deviceId)->default_value(0), "GPU Device index")
+            ("platform-id", po::value<int>(&simulation.platformId)->default_value(0), "Platform index (OpenCL only)")
+            ("platform", po::value<string>(&simulation.platform)->default_value("OpenCL"),
+             "Platform name (CPU, OpenCL, or CUDA)")
+            ("precision", po::value<string>(&simulation.precision)->default_value("single"),
+             "Precision (single or double)")
+            ("workunit,w", po::value<string>()->default_value("dhfr"), "Work unit (WU) name.")
+            ("system", po::value<string>()->default_value(""), "Path to system xml file")
+            ("state", po::value<string>()->default_value(""), "Path to state xml file")
+            ("integrator", po::value<string>()->default_value(""), "Path to integrator xml file")
+            ("disable-accuracy-check", "Don't check against the reference platform")
+            ("enable-accuracy-check", "Check against the reference platform (default)")
+            ("nan-check", po::value<int>(&simulation.nan_check_freq)->default_value(0),
+             "Frequency to perform NaN checks during benchmark. 0 - disable.")
+            ("run-length", po::value<int>(&run_length_sec)->default_value(60), "Benchmark for this many seconds")
+            ("step-chunk", po::value<int>(), "Run this many steps at a time before updating progress.");
 
     po::variables_map vm;
     try {
@@ -168,24 +143,24 @@ int main(int argc, char ** argv) {
     }
 
     if (
-        vm["system"].as<string>() != "" ||
-        vm["integrator"].as<string>() != "" ||
-        vm["state"].as<string>() != ""
-    ) {
+            vm["system"].as<string>() != "" ||
+            vm["integrator"].as<string>() != "" ||
+            vm["state"].as<string>() != ""
+            ) {
         if (!(
-                    vm["system"].as<string>() != "" &&
-                    vm["integrator"].as<string>() != "" &&
-                    vm["state"].as<string>() != "" &&
-                    vm.count("step-chunk")
-                )) {
+                vm["system"].as<string>() != "" &&
+                vm["integrator"].as<string>() != "" &&
+                vm["state"].as<string>() != "" &&
+                vm.count("step-chunk")
+        )) {
             std::cerr << "Please specify all of system, integrator, state, step-chunk" << std::endl;
             return -2;
         }
-        simulation.work_unit = WorkUnit(vm["system"].as<string>() ,
-                                        vm["integrator"].as<string>() ,
+        simulation.work_unit = WorkUnit(vm["system"].as<string>(),
+                                        vm["integrator"].as<string>(),
                                         vm["state"].as<string>(),
                                         vm["step_chunk"].as<int>()
-                                       );
+        );
     } else {
         WorkUnit wu(vm["workunit"].as<string>());
         if (vm.count("step-chunk")) {
@@ -199,10 +174,8 @@ int main(int argc, char ** argv) {
     CommandLineUpdater updater;
     try {
         SimulationResult result = simulation.run(updater);
-        updater.message(boost::format(
-                            "Final score:  %1$8.4f\n"
-                            "Scaled score: %2$8.4f (%3% atoms)\n"
-                        ) % result.score() % result.scaled_score() % result.n_atoms());
+        updater.message(boost::format("Final score:  %1$8.4f\nScaled score: %2$8.4f (%3% atoms)\n")
+                        % result.score() % result.scaled_score() % result.n_atoms());
     } catch (const std::exception & e) {
         std::cout << std::endl << "Something went wrong:" << std::endl;
         std::cout << e.what() << std::endl << std::endl;
