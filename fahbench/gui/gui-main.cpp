1b4279c8ea8d623cd5036cc01a86a29aa986b53e
Matthew Harrigan <harrigan@stanford.edu>
2017-02-24 10:08:20 -0800

    Nicer style on linux

--- fahbench/gui/gui-main.cpp
+++ fahbench/gui/gui-main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QString>
 #include <QIcon>
+#include <QStyleFactory>
 
 #include "FAHBenchVersion.h"
 #include "MainWindow.h"
@@ -10,6 +11,11 @@ int main(int argc, char * argv[]) {
     app.setOrganizationName("Folding@Home");
     app.setApplicationName("FAHBench");
     app.setWindowIcon(QIcon(":FahIcon.ico"));
+#ifdef _WIN32
+    app.setStyle(QStyleFactory::create("windows"));
+#else
+    app.setStyle(QStyleFactory::create("fusion"));
+#endif
 
     app.setApplicationVersion(QString::fromStdString(getVersion()));
     MainWindow main_win;
